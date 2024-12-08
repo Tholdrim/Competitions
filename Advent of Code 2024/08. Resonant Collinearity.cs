6959ed9f2c14540d440e70414d805f82f9235ef0
Jan Walczyński <38507238+Tholdrim@users.noreply.github.com>
2024-12-08 14:10:43 +0100

    Update 08. Resonant Collinearity.cs

--- Advent of Code 2024/08. Resonant Collinearity.cs
+++ Advent of Code 2024/08. Resonant Collinearity.cs
@@ -3,14 +3,13 @@ using Vector2D = (int X, int Y);
 var lines = File.ReadAllLines("Input 08.txt");
 var bounds = new Vector2D(lines[0].Length, lines.Length);
 
-var antennaGroups = lines
-    .Index()
+var antennaGroups = lines.Index()
     .SelectMany(r => r.Item.Index(), (r, c) => new { Antenna = c.Item, Position = new Vector2D(c.Index, r.Index) })
     .Where(e => e.Antenna != '.')
     .GroupBy(e => e.Antenna, e => e.Position);
 
-var antinodesPart1 = new HashSet<Vector2D>();
-var antinodesPart2 = new HashSet<Vector2D>();
+var strictAntinodes = new HashSet<Vector2D>();
+var harmonicAntinodes = new HashSet<Vector2D>();
 
 foreach (var (position, delta) in antennaGroups.SelectMany(FindDeltas))
 {
@@ -20,16 +19,16 @@ foreach (var (position, delta) in antennaGroups.SelectMany(FindDeltas))
     {
         if (i == 1)
         {
-            antinodesPart1.Add((x, y));
+            strictAntinodes.Add((x, y));
         }
 
-        antinodesPart2.Add((x, y));
+        harmonicAntinodes.Add((x, y));
 
         (x, y) = (x + delta.X, y + delta.Y);
     }
 }
 
-var (result1, result2) = (antinodesPart1.Count, antinodesPart2.Count);
+var (result1, result2) = (strictAntinodes.Count, harmonicAntinodes.Count);
 
 Console.WriteLine($"Part 1: {result1}");
 Console.WriteLine($"Part 2: {result2}");
