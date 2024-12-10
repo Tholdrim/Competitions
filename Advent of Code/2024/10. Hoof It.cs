32be5cb0c807797ec090c7a0cceb1c053c21b854
Jan Walczyński <jan.walczynski@perfectio.pl>
2024-12-10 08:04:23 +0100

    Advent of Code 2024 (day 10)

---
+++ Advent of Code/2024/10. Hoof It.cs
@@ -0,0 +1,82 @@
+namespace AdventOfCode.Year2024
+{
+    [TestClass]
+    public class Day10
+    {
+        private static readonly Memory<Vector2D> Directions = new Vector2D[] { new(-1, 0), new(1, 0), new(0, -1), new(0, 1) };
+
+        [TestMethod]
+        [DataRow("Data/Sample 10.txt", 36, 81, DisplayName = "Sample")]
+        [DataRow("Data/Input 10.secret", 737, 1619, DisplayName = "Input")]
+        public void Solve(string fileName, int expectedResult1, int expectedResult2)
+        {
+            var (result1, result2) = (0, 0);
+
+            var map = File.ReadLines(fileName).Select(l => "\0" + l + "\0").ToArray();
+            var borderRow = new string('\0', map[0].Length);
+
+            map = [borderRow, .. map, borderRow];
+
+            for (var y = 1; y < map.Length - 1; ++y)
+            {
+                for (var x = 1; x < map[y].Length - 1; ++x)
+                {
+                    if (map[y][x] == '0')
+                    {
+                        var (score, rating) = CalculateTrailheadMetrics(map, x, y);
+
+                        result1 += score;
+                        result2 += rating;
+                    }
+                }
+            }
+
+            Assert.AreEqual(expectedResult1, result1);
+            Assert.AreEqual(expectedResult2, result2);
+        }
+
+        private static (int Score, int Rating) CalculateTrailheadMetrics(ReadOnlySpan<string> map, int x, int y)
+        {
+            var (score, rating) = (0, 0);
+            var stack = new Stack<(Vector2D Position, char Height)>([new(new Vector2D(x, y), '0')]);
+            Span<bool> visitedPeaks = stackalloc bool[map.Length * map[0].Length];
+
+            visitedPeaks.Clear();
+
+            while (stack.TryPop(out var item))
+            {
+                if (item.Height == '9')
+                {
+                    if (!visitedPeaks[item.Position.Y * map.Length + item.Position.X])
+                    {
+                        ++score;
+                    }
+
+                    ++rating;
+                    visitedPeaks[item.Position.Y * map.Length + item.Position.X] = true;
+
+                    continue;
+                }
+
+                var nextHeight = (char)(item.Height + 1);
+
+                foreach (var direction in Directions.Span)
+                {
+                    var nextPosition = item.Position + direction;
+
+                    if (map[nextPosition.Y][nextPosition.X] == nextHeight)
+                    {
+                        stack.Push(new(nextPosition, nextHeight));
+                    }
+                }
+            }
+
+            return (score, rating);
+        }
+
+        private readonly record struct Vector2D(int X, int Y)
+        {
+            public static Vector2D operator +(Vector2D a, Vector2D b) => new(a.X + b.X, a.Y + b.Y);
+        }
+    }
+}
