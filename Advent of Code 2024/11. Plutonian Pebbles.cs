a9d78ab3385063821ce9482274d67f758739ad2f
Jan Walczyński <38507238+Tholdrim@users.noreply.github.com>
2024-12-11 16:33:01 +0100

    Update 11. Plutonian Pebbles.cs

--- Advent of Code 2024/11. Plutonian Pebbles.cs
+++ Advent of Code 2024/11. Plutonian Pebbles.cs
@@ -1,47 +1,60 @@
-var (result1, result2) = (0L, 0L);
-
 var input = File.ReadAllText("Input 11.txt");
 var stones = input.Split(' ').Select(long.Parse).CountBy(x => x).ToDictionary(x => x.Key, x => (long)x.Value);
 
-for (var i = 0; i < 75; ++i)
+var results = CountStones(stones, 25, 75).ToList();
+
+if (results is not [var result1, var result2])
 {
-    var newStones = new Dictionary<long, long>();
-
-    foreach (var (stone, count) in stones)
-    {
-        if (stone == 0)
-        {
-            newStones[1] = newStones.GetValueOrDefault(1) + count;
-
-            continue;
-        }
-
-        var digits = CountDigits(stone);
-
-        if (digits % 2 == 0)
-        {
-            var power = (long)Math.Pow(10.0, digits / 2);
-
-            newStones[stone / power] = newStones.GetValueOrDefault(stone / power) + count;
-            newStones[stone % power] = newStones.GetValueOrDefault(stone % power) + count;
-        }
-        else
-        {
-            newStones[stone * 2024L] = newStones.GetValueOrDefault(stone * 2024L) + count;
-        }
-    }
-
-    stones = newStones;
-
-    if (i == 24)
-    {
-        result1 = stones.Sum(x => x.Value);
-    }
+    throw new Exception("Implementation error.");
 }
-
-result2 = stones.Sum(x => x.Value);
 
 Console.WriteLine($"Part 1: {result1}");
 Console.WriteLine($"Part 2: {result2}");
 
-static int CountDigits(long number) => (int)(Math.Log10(number) + 1);
+static IEnumerable<long> CountStones(IDictionary<long, long> stones, params IList<int> blinks)
+{
+    var upperBound = blinks.Max();
+
+    for (var i = 0; i < upperBound; ++i)
+    {
+        var newStones = new Dictionary<long, long>();
+
+        foreach (var (stone, count) in stones)
+        {
+            if (stone == 0)
+            {
+                newStones[1] = newStones.GetValueOrDefault(1) + count;
+
+                continue;
+            }
+
+            var digits = CountDigits(stone);
+
+            if (digits % 2 != 0)
+            {
+                var newStone = stone * 2024L;
+
+                newStones[newStone] = newStones.GetValueOrDefault(newStone) + count;
+
+                continue;
+            }
+
+            var power = (long)Math.Pow(10.0, digits / 2);
+
+            var leftStone = stone / power;
+            var rightStone = stone % power;
+
+            newStones[leftStone] = newStones.GetValueOrDefault(leftStone) + count;
+            newStones[rightStone] = newStones.GetValueOrDefault(rightStone) + count;
+        }
+
+        if (blinks.Contains(i + 1))
+        {
+            yield return newStones.Sum(x => x.Value);
+        }
+
+        stones = newStones;
+    }
+}
+
+static int CountDigits(long number) => (int)Math.Log10(number) + 1;
