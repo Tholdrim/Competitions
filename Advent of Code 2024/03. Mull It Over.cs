16bd8494c634de1c0cf2bed5b917e964afc6416c
Jan Walczyński <jan.walczynski@perfectio.pl>
2024-12-14 20:14:06 +0100

    Updates

--- Advent of Code 2024/03. Mull It Over.cs
+++ Advent of Code 2024/03. Mull It Over.cs
@@ -1,13 +1,15 @@
 using System.Text.RegularExpressions;
 
-var (result1, result2, enabled) = (0, 0, true);
-var input = await File.ReadAllTextAsync("Input 03.txt");
+var allowMultiply = true;
+var (result1, result2) = (0, 0);
+
+var input = File.ReadAllText("Input 03.txt");
 
 foreach (var match in Regex.Matches(input).OfType<Match>())
 {
     if (match.Groups["instruction"].Success)
     {
-        enabled = match.Groups["instruction"].Value == "do()";
+        allowMultiply = match.Groups["instruction"].Value == "do()";
 
         continue;
     }
@@ -15,13 +17,13 @@ foreach (var match in Regex.Matches(input).OfType<Match>())
     var product = int.Parse(match.Groups["x"].Value) * int.Parse(match.Groups["y"].Value);
 
     result1 += product;
-    result2 += enabled ? product : 0;
+    result2 += allowMultiply ? product : 0;
 }
 
 Console.WriteLine($"Part 1: {result1}");
 Console.WriteLine($"Part 2: {result2}");
 
-partial class Program
+internal partial class Program
 {
     [GeneratedRegex(@"mul\((?<x>\d{1,3}),(?<y>\d{1,3})\)|(?<instruction>do\(\)|don't\(\))")]
     private static partial Regex Regex { get; }
--- Advent of Code 2024/05. Print Queue.cs
+++ Advent of Code 2024/05. Print Queue.cs
@@ -1,6 +1,6 @@
 var (result1, result2) = (0, 0);
 
-var lines = await File.ReadAllLinesAsync("Input 05.txt");
+var lines = File.ReadAllLines("Input 05.txt");
 var emptyRowIndex = lines.Index().Where(p => string.IsNullOrEmpty(p.Item)).Select(p => p.Index).First();
 
 var pageOrderingRules = lines
@@ -17,42 +17,30 @@ foreach (var update in updates)
     if (IsUpdateInCorrectOrder(update))
     {
         result1 += update[update.Count / 2];
-    }
-    else
-    {
-        var correctlyOrderedUpdate = FixUpdate(update);
 
-        result2 += correctlyOrderedUpdate[correctlyOrderedUpdate.Count / 2];
+        continue;
     }
+
+    var sortedUpdate = SortUpdate(update);
+
+    result2 += sortedUpdate[sortedUpdate.Count / 2];
 }
 
 Console.WriteLine($"Part 1: {result1}");
 Console.WriteLine($"Part 2: {result2}");
 
-bool IsUpdateInCorrectOrder(IList<int> update)
+bool IsUpdateInCorrectOrder(List<int> update)
 {
-    for (var i = 1; i < update.Count; ++i)
-    {
-        if (Enumerable.Range(0, i).Any(j => pageOrderingRules[update[i]].Contains(update[j])))
-        {
-            return false;
-        }
-    }
-
-    return true;
+    return Enumerable.Range(1, update.Count - 1)
+        .All(i => Enumerable.Range(0, i)
+            .All(j => !pageOrderingRules[update[i]].Contains(update[j])));
 }
 
-IList<int> FixUpdate(IList<int> update)
+IList<int> SortUpdate(List<int> update)
 {
     var result = new List<int>(update.Count);
-    var queue = new Queue<int>(update);
 
-    while (queue.TryDequeue(out var page))
-    {
-        var index = pageOrderingRules[page].Select(p => result.IndexOf(p)).Max();
-
-        result.Insert(index + 1, page);
-    }
+    update.ForEach(p => result.Insert(pageOrderingRules[p].Max(result.IndexOf) + 1, p));
 
     return result;
 }
