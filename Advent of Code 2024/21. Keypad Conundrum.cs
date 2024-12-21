4149d5bd27561a1919fa0aed0e69caa417677eae
Jan Walczyński <38507238+Tholdrim@users.noreply.github.com>
2024-12-21 12:54:09 +0100

    Update 21. Keypad Conundrum.cs

--- Advent of Code 2024/21. Keypad Conundrum.cs
+++ Advent of Code 2024/21. Keypad Conundrum.cs
@@ -5,31 +5,17 @@ namespace AdventOfCode2024
     {
         private static readonly Dictionary<char, Vector2D> NumericButtons = new()
         {
-            ['7'] = new Vector2D(0, 0),
-            ['4'] = new Vector2D(0, 1),
-            ['1'] = new Vector2D(0, 2),
-            ['X'] = new Vector2D(0, 3),
-            ['8'] = new Vector2D(1, 0),
-            ['5'] = new Vector2D(1, 1),
-            ['2'] = new Vector2D(1, 2),
-            ['0'] = new Vector2D(1, 3),
-            ['9'] = new Vector2D(2, 0),
-            ['6'] = new Vector2D(2, 1),
-            ['3'] = new Vector2D(2, 2),
-            ['A'] = new Vector2D(2, 3),
+            ['7'] = new Vector2D(0, 0), ['8'] = new Vector2D(1, 0), ['9'] = new Vector2D(2, 0),
+            ['4'] = new Vector2D(0, 1), ['5'] = new Vector2D(1, 1), ['6'] = new Vector2D(2, 1),
+            ['1'] = new Vector2D(0, 2), ['2'] = new Vector2D(1, 2), ['3'] = new Vector2D(2, 2),
+            ['X'] = new Vector2D(0, 3), ['0'] = new Vector2D(1, 3), ['A'] = new Vector2D(2, 3)
         };
 
         private static readonly Dictionary<char, Vector2D> DirectionalButtons = new()
         {
-            ['X'] = new Vector2D(0, 0),
-            ['<'] = new Vector2D(0, 1),
-            ['^'] = new Vector2D(1, 0),
-            ['v'] = new Vector2D(1, 1),
-            ['A'] = new Vector2D(2, 0),
-            ['>'] = new Vector2D(2, 1),
+            ['X'] = new Vector2D(0, 0), ['^'] = new Vector2D(1, 0), ['A'] = new Vector2D(2, 0),
+            ['<'] = new Vector2D(0, 1), ['v'] = new Vector2D(1, 1), ['>'] = new Vector2D(2, 1)
         };
-
-        private static readonly Dictionary<(string, int), long> Cache = new();
 
         [TestMethod]
         [DataRow("Sample 21.txt", 126384L, 154115708116294L, DisplayName = "Sample")]
@@ -42,13 +28,56 @@ namespace AdventOfCode2024
             {
                 var numericPart = long.Parse(code.Where(char.IsDigit).ToArray());
 
-                result1 += FindShortestKeypadLength(code, [NumericButtons, ..Enumerable.Repeat(DirectionalButtons, 2)]) * numericPart;
-                Cache.Clear();
-                result2 += FindShortestKeypadLength(code, [NumericButtons, ..Enumerable.Repeat(DirectionalButtons, 25)]) * numericPart;
+                result1 += FindShortestKeypadLength(code, [NumericButtons, ..Enumerable.Repeat(DirectionalButtons, 2)], []) * numericPart;
+                result2 += FindShortestKeypadLength(code, [NumericButtons, ..Enumerable.Repeat(DirectionalButtons, 25)], []) * numericPart;
             }
 
             Assert.AreEqual(expectedResult1, result1);
             Assert.AreEqual(expectedResult2, result2);
+        }
+
+        private static long FindShortestKeypadLength(string sequence, Dictionary<char, Vector2D>[] keypads, Dictionary<(string, int), long> cache)
+        {
+            if (cache.TryGetValue((sequence, keypads.Length), out var length))
+            {
+                return length;
+            }
+
+            var position = keypads[0]['A'];
+            var forbiddenPosition = keypads[0]['X'];
+
+            foreach (var button in sequence)
+            {
+                var shortest = long.MaxValue;
+                var targetPosition = keypads[0][button];
+
+                foreach (var move in GetMovements(position, targetPosition, forbiddenPosition))
+                {
+                    if (keypads.Length == 1)
+                    {
+                        shortest = move.Length;
+
+                        break;
+                    }
+
+                    var candidate = FindShortestKeypadLength(move, keypads[1..], cache);
+
+                    if (candidate != -1 && (shortest == long.MaxValue || candidate < shortest))
+                    {
+                        shortest = candidate;
+                    }
+                }
+
+                if (shortest == long.MaxValue)
+                {
+                    return cache[(sequence, keypads.Length)] = -1L;
+                }
+
+                length += shortest;
+                position = targetPosition;
+            }
+
+            return cache[(sequence, keypads.Length)] = length;
         }
 
         private static List<string> GetMovements(Vector2D position, Vector2D targetPosition, Vector2D forbiddenPosition)
@@ -82,50 +111,6 @@ namespace AdventOfCode2024
             }
 
             return result.Count > 0 ? result : ["A"];
-        }
-
-        private static long FindShortestKeypadLength(string sequence, Dictionary<char, Vector2D>[] keypads)
-        {
-            if (Cache.TryGetValue((sequence, keypads.Length), out var length))
-            {
-                return length;
-            }
-
-            var position = keypads[0]['A'];
-            var forbiddenPosition = keypads[0]['X'];
-
-            foreach (var button in sequence)
-            {
-                var targetPosition = keypads[0][button];
-                var shortest = long.MaxValue;
-
-                var movements = GetMovements(position, targetPosition, forbiddenPosition);
-                foreach (var move in movements)
-                {
-                    if (keypads.Length == 1)
-                    {
-                        shortest = move.Length;
-                        break;
-                    }
-
-                    var candidate = FindShortestKeypadLength(move, keypads[1..]);
-
-                    if (candidate != -1 && (shortest == long.MaxValue || candidate < shortest))
-                    {
-                        shortest = candidate;
-                    }
-                }
-
-                if (shortest == long.MaxValue)
-                {
-                    return Cache[(sequence, keypads.Length)] = -1L;
-                }
-
-                length += shortest;
-                position = targetPosition;
-            }
-
-            return Cache[(sequence, keypads.Length)] = length;
         }
 
         internal record Vector2D(int X, int Y);
